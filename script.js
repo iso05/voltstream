0a6622c3b7d60fc02545ecf8d83ab7d611ee6ba7
Isobek <muhammadisojorayev27@gmail.com>
2025-10-12 14:43:12 +0500

    Light style applied

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const hamburger = document.querySelector(".hamburger")
 const header = document.querySelector(".header")
 const navLinks = document.querySelector(".nav-links")
+const h2 = document.querySelector('.home-text h2');
+const light = document.querySelector('.light');
+
+h2.addEventListener('mousemove', (e) => {
+  const rect = h2.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  light.style.setProperty('--x', x + 'px');
+  light.style.setProperty('--y', y + 'px');
+  light.style.opacity = 1;
+});
+
+h2.addEventListener('mouseleave', () => {
+  light.style.opacity = 0;
+});
+h2.addEventListener('touchstart', (e) => {
+  const touch = e.touches[0];
+  const rect = h2.getBoundingClientRect();
+  const x = touch.clientX - rect.left;
+  const y = touch.clientY - rect.top;
+  light.style.setProperty('--x', x + 'px');
+  light.style.setProperty('--y', y + 'px');
+  light.style.opacity = 1;
+  h2.style.color = "#000"; // qora bo'ladi
+});
+
+h2.addEventListener('touchend', () => {
+  light.style.opacity = 0;
+  h2.style.color = "white"; // oq holatga qaytadi
+});
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   hamburger.innerHTML = navLinks.classList.contains("show")
